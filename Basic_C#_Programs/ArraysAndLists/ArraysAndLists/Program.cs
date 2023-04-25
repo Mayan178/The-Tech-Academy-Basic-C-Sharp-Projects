545f38ca4d7901c87513ae86fb40646fa712beed
Marie Sz <salle_marie@hotmail.com>
2023-04-25 07:18:19 +0100

    Arrays and lists commit

---
+++ Basic_C#_Programs/ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+        //ARRAYS
+        //// declaring a new array called numArray and stating that it will have 5 elements
+        //int[] numArray = new int[5];
+        ////setting each element in the array
+        //numArray[0] = 5;
+        //numArray[1] = 2;
+        //numArray[2] = 10;
+        //numArray[3] = 200;
+        //numArray[4] = 5000;
+
+        //// shorter way to create an array
+        //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };
+
+        ////faster way
+        //int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 };
+
+        ////changing item in array
+        //numArray2[5] = 650;
+
+        //Console.WriteLine(numArray2[5]);
+
+        //LISTS
+        //List<int> intList = new List<int>();
+        //intList.Add(4);
+        //intList.Add(10);
+        //intList.Remove(10);
+
+        List<string> strList = new List<string>();
+        strList.Add("Hello");
+        strList.Add("Jesse");
+        strList.Remove("Jesse");
+
+        Console.WriteLine(strList[0]);
+
+        Console.ReadLine();
+        }
+    }
---
+++ Basic_C#_Programs/ConsoleAppArrayAssignment/ConsoleAppArrayAssignment/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+    class Program
+    {
+        static void Main()
+        {
+        //CODE FOR THE STRING ARRAY
+        //setting a one-dimensional array of strings
+        string[] strArray = { "David", "John", "Graham", "Jack" };
+        //set boolean value to define later on if the user has found a valid index number
+        bool isStrFound = false;
+
+        //do the following until the user has entered a valid index number
+        do
+        {
+            //prompt the user to select an index from the string array
+            Console.WriteLine("Please, select an index of the string array, so to see what that index contains:");
+            //store user input as userStrInd
+            int userStrInd = Convert.ToInt32(Console.ReadLine());
+            //if the user input is higher than the length of the array, display error message, otherwise, display answer
+            if (userStrInd > strArray.Length-1)
+            {
+                //write out error message
+                Console.WriteLine("Your number is too high. Try again.\n\n");
+            }
+            //the user's input is valid
+            else
+            {
+                //reset boolean value
+                isStrFound = true;
+                //write out string at position userStrInd from the string array
+                Console.WriteLine("The string at index " + userStrInd + " is " + strArray[userStrInd] + ".\n\n");
+            }
+
+        }
+
+        while (!isStrFound);
+
+
+        //CODE FOR THE INTEGER ARRAY
+        //setting a one-dimensional array of integers
+        int[] intArray = { 0,5,10,15,20,25,30 };
+        //set boolean value to define later on if the user has found a valid index number
+        bool isIntFound = false;
+
+        //do the following until the user has entered a valid index number
+        do
+        {
+            //prompt the user to select an index from the integer array
+            Console.WriteLine("Please, select an index of the integer array, so to see what that index contains:");
+            //store user input as userIntInd
+            int userIntInd = Convert.ToInt32(Console.ReadLine());
+            //if the user input is higher than the length of the array, display error message, otherwise, display answer
+            if (userIntInd > intArray.Length-1)
+            {
+                //write out error message
+                Console.WriteLine("Your number is too high. Try again.\n\n");
+            }
+            //the user's input is valid
+            else
+            {
+                //reset boolean value
+                isIntFound = true;
+                //write out string at position userStrInd from the string array
+                Console.WriteLine(intArray[userIntInd]);
+
+                Console.WriteLine("The integer at index " + userIntInd + " is " + intArray[userIntInd] + ".\n\n");
+            }
+
+        }
+
+        while (!isIntFound);
+
+
+        //CODE FOR THE LIST OF STRINGS
+        //declaring the list of strings
+        List<string> myList = new List<string>() {"one","two","three","four","five"};
+        //set boolean value to define later on if the user has found a valid index number
+        bool isinListFound = false;
+
+        //do the following until the user has entered a valid index number
+        do
+        {
+            //prompt the user to select an index from the integer array
+            Console.WriteLine("Please, select an index of the string list, so to see what that index contains:");
+            //store user input as userIntInd
+            int userListNo= Convert.ToInt32(Console.ReadLine());
+            //if the user input is higher than the length of the list, display error message, otherwise, display answer
+            if (userListNo > myList.Count-1)
+            {
+                //write out error message
+                Console.WriteLine("Your number is too high. Try again.\n\n");
+            }
+            //the user's input is valid
+            else
+            {
+                //reset boolean value
+                isinListFound = true;
+                //write out string at position userStrInd from the string array
+                Console.WriteLine(myList[userListNo]);
+
+                Console.WriteLine("The string at index " + userListNo + " is " + myList[userListNo] + ".\n\n");
+            }
+
+        }
+
+        while (!isinListFound);
+
+
+        Console.ReadLine();
+
+        }
+    }
