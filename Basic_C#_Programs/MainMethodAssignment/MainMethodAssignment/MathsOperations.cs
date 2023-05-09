f6a1b8a38a6c1b5331ade64bee7c0c3991d8c03a
Marie Sz <salle_marie@hotmail.com>
2023-05-09 10:50:37 +0100

    Main Method Assignment Commit

---
+++ Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/MathsOperations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class MathsOperations
+    {
+        //method to multiply number supplied by the user by 100
+        public static int CalculateNumbers(int userNumber)
+        {
+            int MultiplyResult = userNumber * 100;
+            return MultiplyResult;
+        }
+
+        //method to substract 10 to a decimal number provided by the user
+        public static int CalculateNumbers(decimal userNumberDec)
+        {
+            decimal SubstractResult = userNumberDec - 10;
+            int newSubstractResult = Convert.ToInt32(SubstractResult);
+            return newSubstractResult;
+        }
+
+        //method to add 20 to one provided by the user as a string
+        public static int CalculateNumbers(string userNumberStr)
+        {
+            int userNumberasInt = Convert.ToInt32(userNumberStr);
+
+            int AddResult = userNumberasInt + 20;
+            return AddResult;
+        }
+
+    }
+}
---
+++ Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //asking the user to input an integer, then we multiply that number by 100 and return it to the user
+           
+            //using error handling to make sure the user inputs a number and not text
+            try
+            {
+                //asking the user to input a number
+                Console.WriteLine("Please enter a number.");
+                //storing that number as userNumber
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+
+                //now calling on the MathsOperations class
+                //calling the method CalculateNumbers
+                int MultiplyResult = MathsOperations.CalculateNumbers(userNumber);
+                //showing result to the user
+                Console.Write(userNumber + " multiplied by 100 is equal to " + MultiplyResult + "\n");
+
+                Console.ReadLine();
+            }
+
+            catch (FormatException ex)
+            {
+                //Console.WriteLine(ex.Message);
+                Console.WriteLine("Please type a whole number.");
+                Console.ReadLine();
+            }
+
+            
+            //asking the user to input a decimal number, then we take away 10 and return the result to the user as an integer
+            //using error handling to make sure the user inputs a decimal number and not something else
+            try
+            {
+                //asking the user to input a decimal number
+                Console.WriteLine("Please enter a decimal number.");
+                //storing that number as userNumberDec
+                decimal userNumberDec = decimal.Parse(Console.ReadLine());
+
+                //now calling on the MathsOperations class
+                //calling the method CalculateNumbers
+                int TakeAwayResult = MathsOperations.CalculateNumbers(userNumberDec);
+                //showing result to the user
+                Console.Write(userNumberDec + " take away 10 is equal to " + TakeAwayResult + "\n");
+
+                Console.ReadLine();
+            }
+
+            catch (FormatException ex)
+            {
+                //Console.WriteLine(ex.Message);
+                Console.WriteLine("Please type a decimal number.");
+                Console.ReadLine();
+            }
+
+
+            //asking the user to input a number as text, then we add 20 to it and return the result to the user
+            //using error handling to make sure the user inputs a string and not something else
+            try
+            {
+                //asking the user to input a number as text
+                Console.WriteLine("Please enter a number.");
+                //storing that number as userNumberDec
+                string userNumberStr = Console.ReadLine();
+
+                //now calling on the MathsOperations class
+                //calling the method CalculateNumbers
+                int AddingResult = MathsOperations.CalculateNumbers(userNumberStr);
+                //showing result to the user
+                Console.Write(userNumberStr + " plus 20 is equal to " + AddingResult + "\n");
+
+                Console.ReadLine();
+            }
+
+            catch (FormatException ex)
+            {
+                //Console.WriteLine(ex.Message);
+                Console.WriteLine("Please enter a number.");
+                Console.ReadLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
