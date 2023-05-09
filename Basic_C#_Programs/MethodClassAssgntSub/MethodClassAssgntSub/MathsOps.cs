7b1c345f86e2662b3bec23d8ce6b1c9aebcc0c32
Marie Sz <salle_marie@hotmail.com>
2023-05-09 12:12:03 +0100

    method class Assignment submission commit

---
+++ Basic_C#_Programs/MethodClassAssgntSub/MethodClassAssgntSub/MathsOps.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssgntSub
+{
+    class MathsOps
+    {
+        //setting up a void method that takes in 2 integers as parameters (we are setting default values for those 2 integers)
+        public static void TakeAwaySixty(int numberOne = 50, int numberTwo = 100)
+        {
+            //do an operation on the first number
+            int myResult = numberOne - 60;
+            //display the second number to the screen
+            Console.WriteLine("Your second number is " + numberTwo);
+            return;
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodClassAssgntSub/MethodClassAssgntSub/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssgntSub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //calling the method passing through 2 numbers
+            MathsOps.TakeAwaySixty(10, 20);
+            //now calling the same method specifying the parameters by name
+            MathsOps.TakeAwaySixty(numberOne: 100, numberTwo: 200);
+            // this stops the console from closing and enables us to see the text 
+           Console.ReadLine();
+        }
+    }
+}
