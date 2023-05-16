65d0a8453fd02f862ff2f7386ac93b26094088fa
Marie Sz <salle_marie@hotmail.com>
2023-05-16 03:43:18 +0100

    Operators Assignment Submission commit

---
+++ Basic_C#_Programs/OperatorsAssignmentSubmission/OperatorsAssignmentSubmission/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignmentSubmission
+{
+    public class Employee
+    {
+        //setting properties for the Employee
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        //use of the == overload operator
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            if (employee1.Equals(employee2))
+                return true;
+            else
+                return false;
+        }
+
+        //use of the != overload operator
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            if (employee1.Equals(employee2))
+                return false;
+            else
+                return true;
+        }
+
+
+    }
+}
---
+++ Basic_C#_Programs/OperatorsAssignmentSubmission/OperatorsAssignmentSubmission/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignmentSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //setting up a first employee named Michael;
+            Employee Michael = new Employee();
+            Michael.Id = 1;
+            Michael.firstName = "Michael";
+            Michael.lastName = "Wilkinson";
+
+            //setting up a second employee named Carole with a different Id to Michael;
+            Employee Carole = new Employee();
+            Carole.Id = 2;
+            Carole.firstName = "Carole";
+            Carole.lastName = "Smith";
+
+            //setting up a third employee named MichaelSecond with same Id as Michael;
+            Employee MichaelSecond = new Employee();
+            MichaelSecond.Id = 1;
+            MichaelSecond.firstName = "Michael";
+            MichaelSecond.lastName = "Wilkinson";
+
+            //checking if Michael and MichaelSecond have the same Id and showing different message depending upon result
+            Console.WriteLine(Michael.Id == MichaelSecond.Id ? "Michael and MichaelSecond have the same Id" : "Michael and MichaelSecond do not have the same Id");
+            //checking if Michael and Carole have a different Id and showing different message depending upon result 
+            Console.WriteLine(Michael.Id != Carole.Id ? "Michael and Carole have different Ids" : "Michael and Carole have the same Id");
+
+
+            Console.ReadLine();
+        }
+    }
+}
