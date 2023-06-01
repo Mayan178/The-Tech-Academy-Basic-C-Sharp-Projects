b4930745779c8e931d0e8670478b2d393b284f7b
Marie Sz <salle_marie@hotmail.com>
2023-06-01 18:03:01 +0100

    MVC commit

---
+++ Basic_C#_Programs/MVCTutorial/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using MVCTutorial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTutorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //logging in data when page visited
+            //string text = "Hello";
+            //System.IO.File.WriteAllText(@"C:\Users\LENOVO-THINKPAD-T430\Desktop\The-Tech-Academy-Basic-C-Sharp-Projects\text-log.txt", text);
+            //
+
+            //printing out random number
+            //Random rnd = new Random(10);
+            //var num = rnd.Next();
+            //ViewBag.RandomNumber = num;
+            //displaying a different page (About page) if number above 20000
+            //if (num > 20000)
+            //{
+            //    return View("About");
+            //}
+
+            //returning the word "hello"
+            //return Content("hello");
+
+            //returning a redirect to the Contact page
+            //return RedirectToAction("Contact");
+
+            //returning the content of the contact page, but still on the index page
+            //return View("Contact");
+
+            //testing data from a list instead of using the Model folder
+            //List<string> names = new List<string>
+            //{
+            //    "Jesse",
+            //    "Adam",
+            //    "Brett"
+            //};
+            //return View(names);
+
+            //passing through a number value instead of using the Model folder
+            //int number = 5;
+            //return View(number);
+
+            User user = new User();
+            user.Id = 1;
+            user.FirstName = "Jesse";
+            user.LastName = "Johnson";
+            user.Age = 32;
+
+            return View(user);
+        }
+        
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            //throwing an error
+            //throw new Exception("Invalid page");
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+          
+            return View();
+        }
+
+       //passing through parameter
+        //public ActionResult Contact(int id = 0)
+        //{
+        //    ViewBag.Message = id;
+
+        //    return View();
+        //}
+    }
+}
--- Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 using Casino;
 using Casino.TwentyOne;
 using System.IO;
+//added for connection to db
+using System.Data.SqlClient;
+using System.Data;
 
 namespace TwentyOne
 {
@@ -31,6 +34,19 @@ namespace TwentyOne
 
             Console.WriteLine("Welcome to the {0}. Let's start by telling me your name.",casinoName);
             string playername = Console.ReadLine();
+
+            if (playername.ToLower() == "admin")
+            {
+                List<ExceptionEntity> Exceptions = ReadExceptions();
+                foreach (var exception in Exceptions)
+                {
+                    Console.Write(exception.Id + " | ");
+                    Console.Write(exception.ExceptionType + " | ");
+                    Console.Write(exception.ExceptionMessage + " | ");
+                    Console.Write(exception.TimeStamp + " | ");
+                    Console.WriteLine();
+                }
+            }
 
             bool validAnswer = false;
             int bank = 0;
@@ -64,16 +80,18 @@ namespace TwentyOne
                     {
                         game.Play();
                     }
-                    catch (FraudException)
+                    catch (FraudException ex)
                     {
-                        Console.WriteLine("Security! Kick this person out!");
+                        Console.WriteLine(ex.Message);
+                        UpdateDbWithException(ex);
                         Console.ReadLine();
                         //stops right here!
                         return;
                     }
-                    catch (Exception)
+                    catch (Exception ex)
                     {
                         Console.WriteLine("An error occurred. Please, contact your System Administrator.");
+                        UpdateDbWithException(ex);
                         Console.ReadLine();
                         //stops right here!
                         return;
@@ -87,6 +105,71 @@ namespace TwentyOne
             Console.Read();
         }
 
-      
+        private static void UpdateDbWithException(Exception ex)
+        {
+            string connectionString = @"Data Source = (localdb)\ProjectsV13; 
+                                    Initial Catalog = TwentyOneGame; 
+                                    Integrated Security = True; 
+                                    Connect Timeout = 30; Encrypt = False; 
+                                    TrustServerCertificate = False; 
+                                    ApplicationIntent = ReadWrite; 
+                                    MultiSubnetFailover = False";
+
+            string queryString = @"INSERT INTO Exceptions (ExceptionType, ExceptionMessage,TimeStamp)" +
+                                 " VALUES (@ExceptionType, @ExceptionMessage, @TimeStamp)";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@ExceptionType", SqlDbType.VarChar);
+                command.Parameters.Add("@ExceptionMessage", SqlDbType.VarChar);
+                command.Parameters.Add("@TimeStamp", SqlDbType.DateTime);
+
+                command.Parameters["@ExceptionType"].Value = ex.GetType().ToString();
+                command.Parameters["@ExceptionMessage"].Value = ex.Message;
+                command.Parameters["@TimeStamp"].Value = DateTime.Now;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+
+        }
+
+      private static List<ExceptionEntity> ReadExceptions()
+        {
+            string connectionString = @"Data Source = (localdb)\ProjectsV13; 
+                                    Initial Catalog = TwentyOneGame; 
+                                    Integrated Security = True; 
+                                    Connect Timeout = 30; Encrypt = False; 
+                                    TrustServerCertificate = False; 
+                                    ApplicationIntent = ReadWrite; 
+                                    MultiSubnetFailover = False";
+
+            string queryString = @"Select Id, ExceptionType, ExceptionMessage, TimeStamp From Exceptions";
+
+            List<ExceptionEntity> Exceptions = new List<ExceptionEntity>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    ExceptionEntity exception = new ExceptionEntity();
+                    exception.Id = Convert.ToInt32(reader["Id"]);
+                    exception.ExceptionType = reader["ExceptionType"].ToString();
+                    exception.ExceptionMessage = reader["ExceptionMessage"].ToString();
+                    exception.TimeStamp = Convert.ToDateTime(reader["TimeStamp"]);
+
+                    Exceptions.Add(exception);
+                }
+                connection.Close();
+            }
+
+            return Exceptions;
+        }
     }
 }
